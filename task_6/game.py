d797735727960c19b7465dd6088e5ca6b50b81d3
Olesia <olesyao2003@gmail.com>
2022-04-15 22:31:56 +0300

    all classes and documentation

--- task_6/game.py
+++ task_6/game.py
@@ -1,92 +1,193 @@
-"""Room, Character, Enemy, Friend, Item for game wanderers"""
+"""Street, Character, Enemy, Friend, Item"""
 
 
 class Street:
+    """Class Street"""
+
     def __init__(self, name: str) -> None:
         self.name = name
         self.connected_rooms = []
         self.item = None
         self.character = None
 
-    def set_description(self, description: str):
+    def set_description(self, description: str) -> None:
+        """
+        Sets description to the room.
+        """
         self.description = description
 
-    def get_details(self):
+    def get_details(self) -> None:
+        """
+        Prints info about current room.
+        """
         print(self.name)
         print('--------------------')
         print(self.description)
         for info in self.connected_rooms:
             room = info[0]
             direction = info[1]
-            print(f'The {room.name} is {direction}')
+            print(f'Вулиця {room.name} - {direction}')
 
-    def link_room(self, room: object, direction: str):
+    def link_room(self, room: object, direction: str) -> None:
+        """
+        Links one room to another.
+        """
         self.connected_rooms.append((room, direction))
 
-    def set_character(self, character: object):
+    def set_character(self, character: object) -> None:
+        """
+        Sets the character in the current room.
+        """
         self.character = character
 
     def get_character(self) -> object:
+        """
+        Returns the character from the current room.
+        """
         return self.character
 
     def set_item(self, item: object):
+        """
+        Sets the item in the current room.
+        """
         self.item = item
 
     def get_item(self) -> object:
+        """
+        Returns the item from the current room.
+        """
         return self.item
 
     def move(self, direction: str) -> object:
+        """
+        Moves to another room.
+        If there is no room in that direction, stays in the current room.
+        """
         for info in self.connected_rooms:
             room = info[0]
             room_direction = info[1]
             if room_direction == direction:
                 return room
-        print('There is no room is that direction.')
+        print('Тупік. Обери інший напрямок.')
         return self
 
 
 class Character:
+    """Class Character"""
+
     def __init__(self, name: str, description: str) -> None:
         self.name = name
         self.description = description
 
-    def describe(self):
-        print(f'{self.name} is here!')
+    def describe(self) -> None:
+        """
+        Prints info about the character.
+        """
+        print(f'Тут {self.name}!')
         print(self.description)
+
+    def set_conversation(self, conversation: str) -> None:
+        """
+        Sets conversation for the character.
+        """
+        self.conversation = conversation
+
+    def talk(self) -> None:
+        """
+        Prints the conversations of the character.
+        """
+        print(f'[{self.name} каже]: {self.conversation}')
 
 
 class Enemy(Character):
+    """Class Enemy - child of class Character"""
     defeated = 0
 
-    def set_conversation(self, conversation: str):
-        self.conversation = conversation
-
-    def set_weakness(self, weapon: str):
+    def set_weakness(self, weapon: str) -> None:
+        """
+        Sets weakness for the character.
+        """
         self.weakness = weapon
 
-    def talk(self):
-        print(f'[{self.name} says]: {self.conversation}')
-
     def fight(self, weapon: str) -> bool:
+        """
+        Returns the result of the fight.
+        True: you won
+        False: enemy won
+        """
         if weapon == self.weakness:
             Enemy.defeated += 1
             return True
-        print(f'{self.name} crushes you, puny adventurer!')
+        print(f'{self.name} розтер вас в порошок :(')
         return False
 
-    def get_defeated(self):
+    def get_defeated(self) -> int:
+        """
+        Returns number of defeated enemys.
+        """
         return Enemy.defeated
 
 
+class Friend(Character):
+    """Class Friend - child of class Character"""
+
+    def set_present(self, present: object):
+        self.present = present
+
+    def get_name_of_present(self) -> str:
+        """
+        Returns name of the present.
+        """
+        return self.present.name
+
+    def get_description_of_present(self) -> str:
+        """
+        Retrns description of the present.
+        """
+        return self.present.description
+
+    def get_present(self) -> None:
+        """
+        Get the present from friend.
+        """
+        print(f"{self.name} подарував тобі {self.get_name_of_present()} - {self.get_description_of_present()}")
+        print(f"{self.get_name_of_present()} тепер в твоєму рюкзаку")
+
+
 class Item:
+    """Class Item"""
+
     def __init__(self, name) -> None:
         self.name = name
 
     def set_description(self, description: str) -> None:
+        """
+        Sets description for the item.
+        """
         self.description = description
 
     def get_name(self) -> str:
+        """
+        Retruns the name of the item.
+        """
         return self.name
 
-    def describe(self):
-        print(f'The [{self.name}] is here - {self.description}')
+    def describe(self) -> None:
+        """
+        Prints description of the item.
+        """
+        print(f'Тут є [{self.name}] - {self.description}')
+
+
+class Weapon(Item):
+    """
+    Class Weapon - child of class Item.
+    """
+    pass
+
+
+class Support(Item):
+    """
+    Class Support - child of class Item.
+    """
+    pass
--- task_6/main.py
+++ task_6/main.py
@@ -1,109 +1,159 @@
+"""Roguelike: Lviv version"""
 import game
 
 
-kozelnytska = game.Street("Kozelnystska")
+kozelnytska = game.Street("Козельницька")
 kozelnytska.set_description("")
 
-stryiska = game.Street("Stryiska")
+stryiska = game.Street("Стрийська")
 stryiska.set_description("")
 
-franka = game.Street("Franka")
+franka = game.Street("Франка")
 franka.set_description("")
 
-shevckenka = game.Street("Shevckenka")
+shevckenka = game.Street("Шевченка")
 shevckenka.set_description("")
 
-krakivska = game.Street("Krakivska")
+krakivska = game.Street("Краківська")
 krakivska.set_description("")
 
-kozelnytska.link_room(stryiska, 'west')
-stryiska.link_room(kozelnytska, 'east')
-kozelnytska.link_room(franka, 'north')
-franka.link_room(kozelnytska, 'south')
-franka.link_room(stryiska, 'west')
-stryiska.link_room(franka, 'north')
-stryiska.link_room(shevckenka, 'west')
-shevckenka.link_room(stryiska, 'east')
-shevckenka.link_room(krakivska, 'north' )
-krakivska.link_room(shevckenka, 'east')
+kozelnytska.link_room(stryiska, 'захід')
+stryiska.link_room(kozelnytska, 'схід')
+kozelnytska.link_room(franka, 'північ')
+franka.link_room(kozelnytska, 'південь')
+franka.link_room(stryiska, 'захід')
+stryiska.link_room(franka, 'північ')
+stryiska.link_room(shevckenka, 'захід')
+shevckenka.link_room(stryiska, 'схід')
+shevckenka.link_room(krakivska, 'північ')
+krakivska.link_room(shevckenka, 'південь')
+
+wine = game.Weapon("пляшка вина")
+wine.set_description("пляшка з-під вина з невідомою субстанцією всередині")
+stryiska.set_item(wine)
+
+sweater = game.Support("укушне худі")
+sweater.set_description("худі, яке зігріє не тільки тіло, а й душу")
+
+shocker = game.Weapon("шокер")
+shocker.set_description("чудова річ для самозахисту і затримання злочинців")
+
+broom = game.Weapon("віник")
+broom.set_description("ним було підметено не одну вулицю")
+franka.set_item(broom)
+
+batyar = game.Enemy("Батяр", "Гульвіса, п'яничка")
+batyar.set_conversation("Агов, де тут найближчий бар?")
+batyar.set_weakness("пляшка вина")
+franka.set_character(batyar)
+
+student = game.Friend("Микола", "Студент УКУ, твій давній знайомий")
+student.set_conversation(
+    "Йоо, давно не бачились! Нарешті можу віддати тобі подарунок - укушне худі)")
+student.set_present(sweater)
+kozelnytska.set_character(student)
+
+laydak = game.Friend("Лайдак", "Вбогий бездомний чоловік")
+laydak.set_conversation("Як же ж холодно на вулиці...")
+laydak.set_present(shocker)
+shevckenka.set_character(laydak)
+
+zbyi = game.Enemy(
+    "Збуй", "Грабіжник, який на твоїх очах вкрав телефон у перехожого")
+zbyi.set_conversation("Чого дивишся? Хочеш неприємностей?!")
+zbyi.set_weakness("шокер")
+krakivska.set_character(zbyi)
 
 
-dave = game.Enemy("Dave", "A smelly zombie")
-dave.set_conversation("What's up, dude! I'm hungry.")
-dave.set_weakness("cheese")
-dining_hall.set_character(dave)
-
-tabitha = game.Enemy("Tabitha", "An enormous spider with countless eyes and furry legs.")
-tabitha.set_conversation("Sssss....I'm so bored...")
-tabitha.set_weakness("book")
-ballroom.set_character(tabitha)
-
-cheese = game.Item("cheese")
-cheese.set_description("A large and smelly block of cheese")
-ballroom.set_item(cheese)
-
-book = game.Item("book")
-book.set_description("A really good book entitled 'Knitting for dummies'")
-dining_hall.set_item(book)
-
-current_room = kitchen
-backpack = []
+current_street = kozelnytska
+backpack = {"support": [], "weapon": []}
 
 dead = False
 
 while dead == False:
 
     print("\n")
-    current_room.get_details()
+    current_street.get_details()
 
-    inhabitant = current_room.get_character()
+    inhabitant = current_street.get_character()
     if inhabitant is not None:
         inhabitant.describe()
 
-    item = current_room.get_item()
+    item = current_street.get_item()
     if item is not None:
         item.describe()
 
     command = input("> ")
 
-    if command in ["north", "south", "east", "west"]:
+    if command in ["північ", "південь", "схід", "захід"]:
         # Move in the given direction
-        current_room = current_room.move(command)
-    elif command == "talk":
+        current_street = current_street.move(command)
+    elif command == "говорити":
         # Talk to the inhabitant - check whether there is one!
         if inhabitant is not None:
             inhabitant.talk()
-    elif command == "fight":
-        if inhabitant is not None:
+            # Get present from your friend
+            if inhabitant == student:
+                student.get_present()
+                backpack["support"].append(student.get_name_of_present())
+                current_street.character = None
+
+            # Help the laydak or ignore him
+            if inhabitant == laydak:
+                print(
+                    "Хочеш подарувати укушне худі лайдаку і врятувати його від холоду?")
+                response = input("так\ні: ")
+                while response != "так" and response != "ні":
+                    response = input("так\ні: ")
+                if response == "так":
+                    if sweater.get_name() in backpack["support"]:
+                        backpack["support"].remove(sweater.get_name())
+                        laydak.get_present()
+                        backpack["weapon"].append(laydak.get_name_of_present())
+                        current_street.character = None
+                    else:
+                        print("Упс, у тебе немає худі. Раджу навідатись до Миколи")
+                else:
+                    print(
+                        "Це звісно твій вибір, але пам'ятай, що все повертається сторицею")
+
+        else:
+            print("Друже, ти говориш сам до себе...")
+    elif command == "битись":
+        if inhabitant is not None and isinstance(inhabitant, game.Enemy):
             # Fight with the inhabitant, if there is one
-            print("What will you fight with?")
+            print("Оберіть знаряддя для битви:")
+            print(backpack["weapon"])
             fight_with = input()
 
             # Do I have this item?
-            if fight_with in backpack:
+            if fight_with in backpack["weapon"]:
 
                 if inhabitant.fight(fight_with) == True:
                     # What happens if you win?
-                    print("Hooray, you won the fight!")
-                    current_room.character = None
+                    print("Єєєй, виграш битви за тобою!")
+                    current_street.character = None
                     if inhabitant.get_defeated() == 2:
-                        print("Congratulations, you have vanquished the enemy horde!")
+                        print(
+                            "Вітаю! Завдяки тобі львівські вулички стали ще безпечнішими!")
                         dead = True
                 else:
                     # What happens if you lose?
-                    print("Oh dear, you lost the fight.")
-                    print("That's the end of the game")
+                    print("Кінець.")
                     dead = True
             else:
-                print("You don't have a " + fight_with)
+                print("У тебе немає " + fight_with)
         else:
-            print("There is no one here to fight with")
-    elif command == "take":
+            print("Спокійно, тут немає з ким битись.")
+    elif command == "взяти":
         if item is not None:
-            print("You put the " + item.get_name() + " in your backpack")
-            backpack.append(item.get_name())
-            current_room.set_item(None)
+            print(item.get_name() + " тепер в твоєму рюкзаку")
+            if isinstance(item, game.Weapon):
+                backpack["weapon"].append(item.get_name())
+            else:
+                backpack["support"].append(item.get_name())
+            current_street.set_item(None)
         else:
-            print("There's nothing here to take!")
+            print("Брати чуже погано!")
     else:
-        print("I don't know how to " + command)
+        print(command + " - незрозуміла команда, спробуй ще раз")
